5ff564dbd40c6c34e2830fbf5d84588841ac1c1a
zhuoyikang <zhuoyikang@gmail.com>
2016-04-26 08:17:17 +0800

    new

--- R/draw_polygon.R
+++ R/draw_polygon.R
@@ -1,14 +1,47 @@
 library(RPostgreSQL)
+drv <- dbDriver("PostgreSQL")
 db <- dbConnect(drv,dbname = "gis_tr",host = "127.0.0.1",port = 5432,user = "zhuoyikang")
-q <- "SELECT id, ST_AsText(geom) FROM polygon_1"
-data <- dbGetQuery(db, q)
+q <- "SELECT id, ST_AsText(geom) FROM polygon_2"
+datastr <- dbGetQuery(db, q)
 
-polygon_vectorstr <- data$st_astext
+polygon_vectorstr <- datastr$st_astext
 
 ## 初始化一个空的polygon列表，里面会有多个polygon.
 polygon_vector <- c()
+finalx <- c()
+finaly <- c()
+i <- 1
 for(polygonstr in polygon_vectorstr) {
-  line_data <- substr(s, 10, nchar(s)-2)
+  line_data <- substr(polygonstr, 10, nchar(polygonstr)-2)
+  line_array <- strsplit(line_data, ",")
+  ## 转变为向量.
+  line_array <- unlist(line_array)
+  cx <- c()
+  cy <- c()
+  lineLength = length(line_array)
+  for(pointstr in line_array) {
+    pointstrarray = strsplit(pointstr, " ")
+    pointstrarray <- unlist(pointstrarray)
+    cx <- c(cx, as.numeric(pointstrarray[1]))
+    cy <- c(cy, as.numeric(pointstrarray[2]))
+
+    finalx <- c(finalx, as.numeric(pointstrarray[1]))
+    finaly <- c(finaly, as.numeric(pointstrarray[2]))
+  }
+  data <- data.frame(seq(1,lineLength,1),cx,cy)
+  polygon_vector <- c(polygon_vector,cx,cy)
+}
+plot(finalx,finaly)
+
+##########################################
+
+polygon_vectorstr <- datastr$st_astext
+
+## 初始化一个空的polygon列表，里面会有多个polygon.
+
+i <- 1
+for(polygonstr in polygon_vectorstr) {
+  line_data <- substr(polygonstr, 10, nchar(polygonstr)-2)
   line_array <- strsplit(line_data, ",")
   ## 转变为向量.
   line_array <- unlist(line_array)
@@ -21,6 +54,10 @@ for(polygonstr in polygon_vectorstr) {
     cx <- c(cx, as.numeric(pointstrarray[1]))
     cy <- c(cy, as.numeric(pointstrarray[2]))
   }
-  data <- data.frame(seq(1,lineLength,1),cx,cy)
-  polygon_vector <- c(polygon_vector,data)
+  polygon(cx, cy,  border = "red")
 }
+
+
+viewX <-c(1.5, 1.5, 2.5, 2.5, 1.5)
+viewY <-c(2.5, 3.5, 3.5, 2.5, 2.5)
+polygon(viewX, viewY,  border = "green")
